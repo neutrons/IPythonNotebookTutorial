c599aaf9a8c5f199faf737666c0b5d3abade5ed1
Jiao Lin <linjiao@ornl.gov>
2018-04-09 20:43:01 -0400

    copy-notebooks.py: copy solutions too

--- copy-notebooks.py
+++ copy-notebooks.py
@@ -4,19 +4,27 @@ from __future__ import print_function
 import getpass, os
 user = getpass.getuser()
 site_url = "https://jupyter.sns.gov"
-target_dir = "notebooks/IPythonNotebookTutorial-notebooks"
-
 nb_dir = os.path.expanduser('~/notebooks')
 if not os.path.exists(nb_dir): os.makedirs(nb_dir)
-target_path = os.path.join(os.path.expanduser('~'), target_dir)
-cmd = "rsync -av /EXAMPLES/IPythonNotebookTutorial/notebooks/ %s/" % target_path
-os.system(cmd)
 
-print()
 BOLD = '\033[1m'
 UNBOLD = '\033[0m'
-print('*'*80)
-print(BOLD+"Open the following link to open the tutorials"+UNBOLD)
+def copy(subdir, description):
+    target_dir = "notebooks/IPythonNotebookTutorial-%s" % subdir
+    target_path = os.path.join(os.path.expanduser('~'), target_dir)
+    cmd = "rsync -q -av /EXAMPLES/IPythonNotebookTutorial/%s/ %s/" % (subdir, target_path)
+    if os.system(cmd):
+        raise RuntimeError("%s failed" % cmd)
+    print(BOLD+description+UNBOLD+": %s/user/%s/tree/%s/" % (site_url, user, target_dir))
+    return
+
+print('*'*95)
 print()
-print("%s/user/%s/tree/%s/" % (site_url, user, target_dir))
-print('*'*80)
+print(BOLD+"Open the following links:"+UNBOLD)
+print()
+copy('notebooks', 'tutorials')
+copy('solutions', 'solutions')
+
+print()
+print('*'*95)
+
